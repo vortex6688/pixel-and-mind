aaa709776dba882f71045b22b8abcccffe0511a3
VORTEX <vortex6688@gmail.com>
2017-12-05 23:22:32 -0500

    clear up the repo

--- src/app/development/development.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { DevelopmentComponent } from './index';
-
-export const DevelopmentRoutes: Route[] = [
-  {
-    path: 'development',
-    component: DevelopmentComponent
-  }
-];
