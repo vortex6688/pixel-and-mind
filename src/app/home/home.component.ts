723cf2928b83307c97cb049470be306bdd055422
VORTEX <vortex6688@gmail.com>
2017-12-06 03:31:17 -0500

    deploy

--- src/app/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-})
-export class HomeComponent {}
